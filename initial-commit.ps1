08b30c1f2f40c0417b9d16fa54b07eb156229d2e
matheus <matheus.felipe.19rt@gmail.com>
2024-06-30 18:28:59 -0300

    feat: Add initial-commit script

---
+++ initial-commit.ps1
@@ -0,0 +1,15 @@
+# Set GitHub username from environment variable
+$githubUsername = $env:GITHUB_USERNAME
+
+# Prompt for GitHub repository name
+$repoName = Read-Host -Prompt "Enter GitHub repository name"
+
+# Set the repository URL using GitHub username & GitHub repository name
+$repoUrl = "https://github.com/$githubUsername/$repoName.git"
+
+# Add the remote origin, add files, commit, and push to main branch
+git remote add origin $repoUrl
+git add .
+git commit -m "Initial Commit"
+git branch -M main
+git push -u origin main
