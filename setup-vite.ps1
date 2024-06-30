adb71e5460d2158470dd07a1a768ec1ed777941d
matheus <matheus.felipe.19rt@gmail.com>
2024-06-30 14:57:26 -0300

    Initial Commit

---
+++ setup-vite.ps1
@@ -0,0 +1,18 @@
+git clone https://github.com/w11dcard/boilerplate-vite .
+git remote remove origin
+Remove-Item README.md
+New-Item .env
+npm install
+
+$repoName = Read-Host -Prompt "Enter GitHub repository name"
+$jsonData = @{
+    name = $repoName
+    private = $true
+} | ConvertTo-Json
+
+$headers = @{
+    Authorization = "token $($env:GITHUB_TOKEN)"
+    Accept = "application/vnd.github.v3+json"
+}
+
+Invoke-RestMethod -Uri "https://api.github.com/user/repos" -Method Post -Headers $headers -Body $jsonData
